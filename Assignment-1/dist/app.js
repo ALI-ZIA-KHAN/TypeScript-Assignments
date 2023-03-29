7388d074970b9e40071bbaec0e142d1cf9e2bbd0
ALI-ZIA-KHAN <azk443.443@gmail.com>
2023-03-29 13:46:23 +0500

    added elaboration

--- Assignment-1/dist/app.js
+++ Assignment-1/dist/app.js
@@ -10,20 +10,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const callingAPI = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        //. First, you need to choose a public API to fetch data from. There are many
+        //First, you need to choose a public API to fetch data from. There are many
         // public APIs available that provide different types of data such as weather,
         // news, sports, and more. Choose an API that provides data in a tabular
         // format.
         const res = yield fetch('https://dummyjson.com/products');
-        //  
+        //Use the XMLHttpRequest object to fetch data from the API. You can also
+        //use the fetch API, which is available in modern browsers
         const data = yield res.json();
+        // Once you have retrieved the data, parse it into a JavaScript object. You can
+        // use JSON.parse() to parse JSON data.
         console.log("data", data.products);
         if (!data) {
             // throw new Error('Data is not present');
             alert("Oops there is problem in connecting with API");
         }
         createTable(data.products);
-        //       Next, create a TypeScript file, say index.ts, in which you will write the code
+        // Next, create a TypeScript file, say index.ts, in which you will write the code
         // to fetch data from the API and display it in a table
     }
     catch (error) {
@@ -31,6 +34,9 @@ const callingAPI = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 function createTable(products) {
+    // Create an HTML table dynamically using the data you retrieved. You can
+    // create a table element and dynamically add tr and td elements to it using
+    // JavaScript.
     const table = document.createElement('table');
     table.setAttribute("class", "table_class");
     table.setAttribute("id", "products");
@@ -44,12 +50,10 @@ function createTable(products) {
         th.appendChild(text);
         headRow.appendChild(th);
     });
-    // Loop through the product data and create table rows
     products.forEach((product) => {
         console.log("p", product);
         const tr = tbody.insertRow();
         tr.setAttribute("class", "table_class");
-        // Insert the data for each product into the table cells
         tr.insertCell().appendChild(document.createTextNode(product.id.toString()));
         tr.insertCell().appendChild(document.createTextNode(product.title));
         tr.insertCell().appendChild(document.createTextNode(product.brand));
@@ -59,7 +63,7 @@ function createTable(products) {
         tr.insertCell().appendChild(document.createTextNode(product.rating.toString()));
         tr.insertCell().appendChild(document.createTextNode(product.discountPercentage.toString()));
     });
-    // Add the table in browser
+    // Finally, append the table to the DOM so that it is visible on the webpage.
     document.body.appendChild(table);
 }
 callingAPI();
--- Assignment-1/src/app.ts
+++ Assignment-1/src/app.ts
@@ -4,17 +4,21 @@
 const callingAPI = async () => {
     try {
 
-
-
-//. First, you need to choose a public API to fetch data from. There are many
+//First, you need to choose a public API to fetch data from. There are many
 // public APIs available that provide different types of data such as weather,
 // news, sports, and more. Choose an API that provides data in a tabular
 // format.
       const res = await fetch('https://dummyjson.com/products');
 
-//  
+//Use the XMLHttpRequest object to fetch data from the API. You can also
+//use the fetch API, which is available in modern browsers
+
+
+
 
       const data = await res.json();
+// Once you have retrieved the data, parse it into a JavaScript object. You can
+// use JSON.parse() to parse JSON data.
       console.log("data",data.products)
    
       if (!data) {
@@ -24,7 +28,7 @@ const callingAPI = async () => {
   
       createTable(data.products);
 
-//       Next, create a TypeScript file, say index.ts, in which you will write the code
+// Next, create a TypeScript file, say index.ts, in which you will write the code
 // to fetch data from the API and display it in a table
     } catch (error) {
       console.log('Error:', error);
@@ -46,7 +50,9 @@ const callingAPI = async () => {
   
   function createTable(products: Product[]) {
 
-
+    // Create an HTML table dynamically using the data you retrieved. You can
+    // create a table element and dynamically add tr and td elements to it using
+    // JavaScript.
     const table = document.createElement('table');
     table.setAttribute("class","table_class");
     table.setAttribute("id","products")
@@ -63,13 +69,13 @@ const callingAPI = async () => {
       headRow.appendChild(th);
     });
   
-    // Loop through the product data and create table rows
+    
     products.forEach((product) => {
         console.log("p",product)
       const tr = tbody.insertRow();
       tr.setAttribute("class","table_class")
   
-      // Insert the data for each product into the table cells
+      
       tr.insertCell().appendChild(document.createTextNode(product.id.toString()));
       tr.insertCell().appendChild(document.createTextNode(product.title));
       tr.insertCell().appendChild(document.createTextNode(product.brand));
@@ -81,7 +87,7 @@ const callingAPI = async () => {
       
     });
   
-    // Add the table in browser
+    // Finally, append the table to the DOM so that it is visible on the webpage.
     document.body.appendChild(table);
   }
   
